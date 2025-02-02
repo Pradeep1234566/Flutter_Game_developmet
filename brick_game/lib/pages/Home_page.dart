ebfe6a2bd90a0de74685cc759f657638dd25b18e
Pradeep <pawarpradeep1707@gmail.com>
2025-02-02 11:57:49 +0530

    Added A timer to the game

--- brick_game/lib/pages/Home_page.dart
+++ brick_game/lib/pages/Home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,23 +10,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double x = 0;
+  double y = 0;
+
+  void startGame() {
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        y -= 0.01;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.deepPurple[200],
-      body: Stack(children: [
-        Container(
-          alignment: Alignment(0, 0),
-          child: Container(
-            height: 15,
-            width: 15,
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              borderRadius: BorderRadius.circular(50),
+    return GestureDetector(
+      onTap: startGame,
+      child: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: Stack(children: [
+          Container(
+            alignment: Alignment(0, 0),
+            child: Container(
+              height: 15,
+              width: 15,
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(50),
+              ),
             ),
-          ),
-        )
-      ]),
+          )
+        ]),
+      ),
     );
   }
 }
