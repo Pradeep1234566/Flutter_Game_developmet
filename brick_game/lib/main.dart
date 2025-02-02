1c3849255fc8dd446232aee1356da068c6f001f0
Pradeep <pawarpradeep1707@gmail.com>
2025-02-02 11:50:56 +0530

    Hmm

--- brick_game/lib/main.dart
+++ brick_game/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:brick_game/pages/Home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: HomePage(),
     
     );
   }
