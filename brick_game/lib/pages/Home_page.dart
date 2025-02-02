6f831a4121a5763a62a5d873836b30ead9fd3037
Pradeep <pawarpradeep1707@gmail.com>
2025-02-02 11:55:04 +0530

    Added background COlor to the home page

--- brick_game/lib/pages/Home_page.dart
+++ brick_game/lib/pages/Home_page.dart
@@ -10,6 +10,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Stack(children: [
+        Container(
+          alignment: Alignment(0, 0),
+          child: Container(
+            height: 15,
+            width: 15,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+        )
+      ]),
+    );
   }
 }
