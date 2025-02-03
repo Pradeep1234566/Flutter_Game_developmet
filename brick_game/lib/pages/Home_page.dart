a44365ce64945903ee4484a3af786947a026f749
Pradeep Pawar <pawarpradeep1707@gmail.com>
2025-02-03 21:04:40 +0530

    Some Aesthetic changes
    Added cool contrast colors and made the ball to falll down instaed of going up should work on the restart option

--- brick_game/lib/pages/Home_page.dart
+++ brick_game/lib/pages/Home_page.dart
@@ -1,7 +1,4 @@
 import 'dart:async';
-import 'package:brick_game/pages/ball_page.dart';
-import 'package:brick_game/pages/cover_page.dart';
-import 'package:brick_game/pages/player_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,22 +9,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Ball position coordinates
-  double x = 0;
-  double y = 0;
-  double ballDirectionX = 1; // 1 for right, -1 for left
-  double ballDirectionY = -1; // -1 for up, 1 for down
-
-  // Game state
+  double x = 0, y = 0;
+  double ballDirectionX = 1,
+      ballDirectionY = 1; // Changed to make the ball fall down initially
   bool isGameStarted = false;
-
-  // Player parameters
-  double playerWidth = 0.2; // Decreased player width
+  double playerWidth = 0.05;
   double playerX = 0;
-
-  final double movementSensitivity = 0.02;
-
-  // Bricks
+  final double movementSensitivity = 0.05;
   List<Brick> bricks = [];
 
   @override
@@ -37,122 +25,106 @@ class _HomePageState extends State<HomePage> {
   }
 
   void createBricks() {
-    bricks = List.generate(10, (i) {
-      // Increased number of bricks
-      return Brick(x: -0.9 + (i % 5) * 0.45, y: -0.5 + (i ~/ 5) * 0.2);
-    });
+    bricks = [];
+    for (double i = -0.9; i < 0.9; i += 0.3) {
+      for (double j = -0.6; j < -0.2; j += 0.15) {
+        bricks.add(Brick(x: i, y: j));
+      }
+    }
   }
 
   void startGame() {
-    setState(() {
-      isGameStarted = true;
-    });
-
+    setState(() => isGameStarted = true);
     Timer.periodic(Duration(milliseconds: 10), (timer) {
       setState(() {
-        // Move ball based on direction
         x += 0.01 * ballDirectionX;
         y += 0.01 * ballDirectionY;
 
-        // Reverse direction when hitting top or bottom
-        if (y <= -1) {
-          ballDirectionY = 1; // Start moving down
-        }
+        if (y <= -1) ballDirectionY = 1;
         if (y >= 0.9) {
-          // Check if ball is within player width
           if (x >= playerX && x <= playerX + playerWidth) {
-            ballDirectionY = -1; // Start moving up
+            ballDirectionY = -1;
           } else {
-            // Game over or reset ball position
             timer.cancel();
             isGameStarted = false;
           }
         }
-
-        // Reverse direction when hitting sides
-        if (x <= -1 || x >= 1) {
-          ballDirectionX *= -1;
-        }
-
-        // Check for collision with bricks
-        for (var brick in bricks) {
+        if (x <= -1 || x >= 1) ballDirectionX *= -1;
+        bricks.removeWhere((brick) {
           if (brick.isHit(x, y)) {
             ballDirectionY *= -1;
-            bricks.remove(brick);
-            break;
+            return true;
           }
-        }
+          return false;
+        });
       });
     });
   }
 
   void moveLeft() {
-    setState(() {
-      if (playerX - movementSensitivity >= -1) {
-        playerX -= movementSensitivity;
-      }
-    });
+    setState(() =>
+        playerX = (playerX - movementSensitivity).clamp(-1, 1 - playerWidth));
   }
 
   void moveRight() {
-    setState(() {
-      if (playerX + movementSensitivity <= 1 - playerWidth) {
-        playerX += movementSensitivity;
-      }
-    });
+    setState(() =>
+        playerX = (playerX + movementSensitivity).clamp(-1, 1 - playerWidth));
   }
 
   @override
   Widget build(BuildContext context) {
-    return KeyboardListener(
-      focusNode: FocusNode(),
-      autofocus: true,
-      child: GestureDetector(
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: GestureDetector(
         onTap: startGame,
         onHorizontalDragUpdate: (details) {
-          double velocity = details.primaryDelta ?? 0;
-          if (velocity > 0) {
+          if (details.primaryDelta! > 0) {
             moveRight();
-          } else if (velocity < 0) {
+          } else {
             moveLeft();
           }
         },
-        child: SafeArea(
-          child: Scaffold(
-            backgroundColor: Colors.deepPurple[200],
-            body: Stack(children: [
-              CoverPage(isGameStarted: isGameStarted),
-              BallPage(x: x, y: y),
-              PlayerPage(player_width: playerWidth, x: playerX),
-              ...bricks
-                  .map((brick) => Positioned(
-                        left: (brick.x + 1) *
-                            MediaQuery.of(context).size.width /
-                            2,
-                        top: (brick.y + 1) *
-                            MediaQuery.of(context).size.height /
-                            2,
-                        child: Container(
-                          width: 100,
-                          height: 20,
-                          color: Colors.red,
-                        ),
-                      ))
-                  .toList(),
-            ]),
+        child: Stack(children: [
+          if (!isGameStarted)
+            Center(
+                child: Text("Tap to Start",
+                    style: TextStyle(color: Colors.white, fontSize: 24))),
+          Positioned(
+            left: (x + 1) * MediaQuery.of(context).size.width / 2,
+            top: (y + 1) * MediaQuery.of(context).size.height / 2,
+            child: Container(
+                width: 15,
+                height: 15,
+                decoration:
+                    BoxDecoration(color: Colors.white, shape: BoxShape.circle)),
           ),
-        ),
+          Positioned(
+            left: (playerX + 1) * MediaQuery.of(context).size.width / 2,
+            bottom: 50,
+            child: Container(
+                width: MediaQuery.of(context).size.width * playerWidth,
+                height: 15,
+                color: Colors.blue),
+          ),
+          ...bricks.map((brick) => Positioned(
+                left: (brick.x + 1) * MediaQuery.of(context).size.width / 2,
+                top: (brick.y + 1) * MediaQuery.of(context).size.height / 2,
+                child: Container(
+                    width: 80,
+                    height: 20,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5))),
+              ))
+        ]),
       ),
     );
   }
 }
 
 class Brick {
-  double x;
-  double y;
-
+  double x, y;
   Brick({required this.x, required this.y});
-
   bool isHit(double ballX, double ballY) {
     return (ballX >= x && ballX <= x + 0.2) && (ballY >= y && ballY <= y + 0.1);
   }
